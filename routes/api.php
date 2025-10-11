bc3446ed3f5431e6fb07c57a57bbacabad863e24
josephjreije35 <josephjreije35@gmail.com>
2025-10-11 08:35:15 +0300

    c

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,14 @@ Route::prefix('image')->group(function () {
     Route::post('/compress', [ImageController::class, 'compress']);
     Route::post('/tune', [ImageController::class, 'tune']);
     Route::post('/convert-to-pdf', [ImageController::class, 'convertToPdf']);
+    Route::post('/convert-to-png', [PdfController::class, 'toPng']);
+    Route::post('/convert-to-jpeg', [PdfController::class, 'toJpeg']);
 });
 
 Route::prefix('pdf')->group(function () {
     Route::post('/split', [PdfController::class, 'split']);
     Route::post('/merge', [PdfController::class, 'merge']);
+    Route::post('/convert-to-img', [PdfController::class, 'pdfsToImages']);
 });
 
 Route::get('/download/{type}/{filename}', [FileController::class, 'download'])->name('files.download');
